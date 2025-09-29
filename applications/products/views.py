2de59b82f8da4248d8f3e2a28511a0d4527d6828
Kelo03 <ezequiel_30_12@hotmail.com>
2025-09-29 13:47:19 -0300

    Se agrego modal, para visualizar pedidos

--- applications/products/views.py
+++ applications/products/views.py
@@ -3,15 +3,10 @@ from django.http import HttpResponse
 from .models import Category, Product
 from django.shortcuts import render, get_object_or_404
 from .forms import AddToCartForm
-# Create your views here.
-
-
-
 
 def product_list(request, category_id=None):
     categories = Category.objects.all()
     
-    # Si no selecciona categoría, mostrar la primera
     if category_id:
         products = Product.objects.filter(category_id=category_id, is_active=True)
         current_category = Category.objects.get(id=category_id)
@@ -26,7 +21,8 @@ def product_list(request, category_id=None):
     }
     return render(request, "products/product_list.html", context)
 
-def product_detail(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    cart_product_form = AddToCartForm()
-    return render(request, "products/product_detail.html", {"product": product, 'cart_product_form':cart_product_form})
+# Podemos comentar temporalmente la vista de detalle
+# def product_detail(request, product_id):
+#     product = get_object_or_404(Product, id=product_id)
+#     cart_product_form = AddToCartForm()
+#     return render(request, "products/product_detail.html", {"product": product, 'cart_product_form': cart_product_form})
