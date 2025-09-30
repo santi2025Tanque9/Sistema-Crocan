d5157f513066b25ae9d95eceffe4d3f7c0b345a2
Kelo03 <ezequiel_30_12@hotmail.com>
2025-09-30 16:56:47 -0300

    Update __init__.cpython-312.pyc, admin.cpython-312.pyc, and 45 more files...

--- crocan/settings/base.py
+++ crocan/settings/base.py
@@ -98,14 +98,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
 
+#USE_I18N = True
+
+#USE_TZ = True
+
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 USE_I18N = True
-
 USE_TZ = True
-
 
 
 
